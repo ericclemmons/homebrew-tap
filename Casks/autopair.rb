7813abe4d4932f150e1d60b7cb392a4a6da236f0
Eric Clemmons <eric@smarterspam.com>
2026-02-28 12:54:07 -0600

    Update autopair to 1.0.0

--- Casks/autopair.rb
+++ Casks/autopair.rb
@@ -1,6 +1,6 @@
 cask "autopair" do
   version "1.0.0"
-  sha256 "PLACEHOLDER"
+  sha256 "5dd6f55ac69ba301e551a2468d4e03ff35ce0e8f49a588d2f2f7c4c38a14dbc9"
 
   url "https://github.com/ericclemmons/autopair/releases/download/v#{version}/AutoPair.zip"
   name "AutoPair"
